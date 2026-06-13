e709caaaf8201ba9b9725814f38b09d81649eecc
arome-prod <prod.arome@gmail.com>
2026-06-13 21:06:40 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ import {
   get,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-import { db, isConfigured } from "./firebase.js?v=137";
-import { DEFAULTS, DEMO, DEMO_INSP } from "./config.js?v=137";
+import { db, isConfigured } from "./firebase.js?v=138";
+import { DEFAULTS, DEMO, DEMO_INSP } from "./config.js?v=138";
 
 const $ = (id) => document.getElementById(id);
 const esc = (s = "") =>
